1d82e69b02d4b507ce5274147470f659537e8354
Michaelkuate <Obakengmichaelkuate>
2022-10-27 14:36:22 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings values
+* @s1: string 1
+* @s2: string 2
+* Return: the difference in s1 and s2
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i;
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+
+return (dest);
+}
